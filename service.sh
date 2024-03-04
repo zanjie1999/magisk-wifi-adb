2e7f804e7c68a3f8b03894b5bc48e5e71cf99021
zanjie1999 <zanjie1999@yeah.net>
2024-03-04 21:23:14 +0800

    咩咩的模块

---
+++ service.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+# 请不要硬编码 /magisk/modname/... ; 请使用 $MODDIR/...
+# 这将使你的脚本更加兼容，即使Magisk在未来改变了它的挂载点
+MODDIR=${0%/*}
+
+settings put global adb_enabled 1
+setprop service.adb.tcp.port 5555
+stop adbd
+start adbd
